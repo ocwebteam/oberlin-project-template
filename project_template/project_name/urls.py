516a87900236c6456c4041e5ed3a7bc859a7bff6
Harris L. <harris@chromamine.com>
2012-07-16 12:21:12 -0300

    Missing an import of settings in the urls.

--- project_template/project_name/urls.py
+++ project_template/project_name/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.conf import settings
 
 admin.autodiscover()
 
